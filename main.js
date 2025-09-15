fddf8bc01f04401642775d779926dca26a2db990
Junseok Hwang <sunday6003@gmail.com>
2025-09-15 21:05:39 +0900

    first commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const apiKey = `d114bde3033b42e2b269078193ce6b0f`;
 let news = [];
 const getLatestNews = async () => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${apiKey}`
     );
     const response = await fetch(url);
     const data = await response.json();
