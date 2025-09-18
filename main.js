cd140d369486d7807d31aba092e1afc385bf6d30
Junseok Hwang <sunday6003@gmail.com>
2025-09-18 09:46:23 +0900

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
 const apiKey = `d114bde3033b42e2b269078193ce6b0f`;
-let news = [];
+let newsList = [];
 const getLatestNews = async () => {
     const url = new URL(
-        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&apiKey=${apiKey}`
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
     );
     const response = await fetch(url);
     const data = await response.json();
-    news = data.articles;
-    console.log("dddd", news);
+    newsList = data.articles;
+    render();
+    console.log("dddd", newsList);
 };
-getLatestNews();
+
+
+const openNav = () => {
+    document.getElementById("mysidenav").style.width = "250px";
+};
+const closeNav =  () => {
+    document.getElementById("mySidenav").style.width = "0";
+}
+const openSearchBox = () => {
+  let inputArea = document.getElementById("input-area");
+  if (inputArea.style.display === "inline") {
+    inputArea.style.display = "none";
+  } else {
+    inputArea.style.display = "inline";
+  }
+};
+
+const render=()=>{
+    const newsHTMl = newsList.map(news=>`<div class="row news">
+            <div class="col-lg-4">
+                <img class="news-img-size news-img" src=${news.urlToImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+}/>
+            </div>
+            <div class="col-lg-8">
+                <h2>${news.title}</h2>
+                <p>
+                    ${news.description}
+                </p>
+                <div>
+                    ${news.source.name}KBS * ${news.publifshedAt}
+                </div>
+            </div>
+        </div>`).join('');
+    console.log("html", newsHTMl)
+    document.getElementById("news-board").innerHTML=newsHTMl
+};
