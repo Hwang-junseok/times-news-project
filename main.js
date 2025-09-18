92867ce1d30c28cc6c318c7a2b986c85d92ffe85
Junseok Hwang <sunday6003@gmail.com>
2025-09-18 17:47:25 +0900

    commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const apiKey = `d114bde3033b42e2b269078193ce6b0f`;
 let newsList = [];
 const getLatestNews = async () => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&apiKey=${apiKey}`
     );
     const response = await fetch(url);
     const data = await response.json();
@@ -30,19 +30,26 @@ const openSearchBox = () => {
 const render=()=>{
     const newsHTMl = newsList.map(news=>`<div class="row news">
             <div class="col-lg-4">
-                <img class="news-img-size news-img" src=${news.urlToImage || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
-}/>
+                <img class="news-img-size news-img" src="${news.urlToImage}" onerror="this.onerror=null; this.src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';"}/>
             </div>
             <div class="col-lg-8">
                 <h2>${news.title}</h2>
                 <p>
-                    ${news.description}
+                    ${news.description == null || news.description == ""
+                        ? "내용없음"
+                        : news.description.length > 200
+                        ?news.description.substring(0, 200) + "..."
+                        :news.description
+                    }
                 </p>
                 <div>
-                    ${news.source.name}KBS * ${news.publifshedAt}
+                    ${news.source.name || "no source"} ${moment(news.publifshedAt).fromNow()}
                 </div>
             </div>
         </div>`).join('');
     console.log("html", newsHTMl)
     document.getElementById("news-board").innerHTML=newsHTMl
 };
+getLatestNews();
+
+ 
